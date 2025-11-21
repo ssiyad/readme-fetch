d1922b7d86292c9de3acc42c00b259129eb87a8c
Sabu Siyad <hello@ssiyad.com>
2025-11-21 21:30:23 +0530

    feat: init tera template

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
+use tera;
+
 fn main() {
     output();
 }
 
 fn output() {
-    println!("{}@{}", user_name(), host());
+    let mut tera = tera::Tera::default();
+    let context = template_context();
+    let template_name = "template";
+    let template_string = include_str!("template.tera");
+    tera.add_raw_template(template_name, template_string).ok();
+    let rendered = tera.render(template_name, &context).unwrap();
+    println!("{}", rendered);
 }
 
-fn user_name() -> String {
+fn template_context() -> tera::Context {
+    let mut context = tera::Context::new();
+    context.insert("user", &user());
+    context.insert("host", &host());
+    context
+}
+
+fn user() -> String {
     "ssiyad".to_string()
 }
 
