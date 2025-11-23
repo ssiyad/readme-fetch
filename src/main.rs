30c3c3e3a4ca9ffd803a7163b8636d1a8642f026
Sabu Siyad <hello@ssiyad.com>
2025-11-23 22:10:45 +0530

    feat: use args instead of env

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,39 @@
 mod sources;
 
 use chrono::prelude::*;
+use clap::Parser;
 use date_differencer;
-use std::env;
 use tera;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Username to show in the output
+    #[arg(long)]
+    user: String,
+
+    /// Hostname to show in the output
+    #[arg(long)]
+    host: String,
+
+    /// GitHub Username to fetch data for
+    #[arg(long)]
+    github_user: String,
+
+    /// GitHub Token for API access
+    #[arg(long)]
+    github_token: String,
+}
 
 #[tokio::main]
 async fn main() {
-    output().await;
+    let args = Args::parse();
+    output(args).await;
 }
 
-async fn output() {
+async fn output(args: Args) {
     let mut tera = tera::Tera::default();
-    let context = template_context().await;
+    let context = template_context(args).await;
     let template_name = "template";
     let template_string = include_str!("template.tera");
     tera.add_raw_template(template_name, template_string).ok();
@@ -20,37 +41,24 @@ async fn output() {
     print!("{}", rendered);
 }
 
-async fn template_context() -> tera::Context {
+async fn template_context(args: Args) -> tera::Context {
     let octo = octocrab::OctocrabBuilder::default()
-        .personal_token(github_token())
+        .personal_token(args.github_token)
         .build()
         .unwrap();
     let mut context = tera::Context::new();
-    context.insert("user", &user());
-    context.insert("host", &host());
+    context.insert("user", &args.user);
+    context.insert("host", &args.host);
     context.insert("uptime", &uptime());
     context.insert(
         "commit_count",
-        &sources::commit_count(&octo, &github_user()).await,
+        &sources::commit_count(&octo, &args.github_user).await,
     );
-    context.insert("pr_count", &sources::pr_count(&octo, &github_user()).await);
+    context.insert(
+        "pr_count",
+        &sources::pr_count(&octo, &args.github_user).await,
+    );
     context
-}
-
-fn github_user() -> String {
-    env::var("GITHUB_USER").expect("GITHUB_USER not set")
-}
-
-fn github_token() -> String {
-    env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN not set")
-}
-
-fn user() -> String {
-    env::var("FETCH_USER").expect("FETCH_USER not set")
-}
-
-fn host() -> String {
-    env::var("FETCH_HOST").expect("FETCH_HOST not set")
 }
 
 fn uptime() -> String {
